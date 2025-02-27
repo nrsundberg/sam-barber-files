744b997a6587410a58191c398b3e065376b0fd3b
Noah Sundberg <noah.sundberg@visionistinc.com>
2025-02-27 09:57:37 -0500

    annotate loader function with Route type

--- app/routes/admin/fileCheck.tsx
+++ app/routes/admin/fileCheck.tsx
@@ -1,4 +1,3 @@
-import prisma from "~/db.server";
 import type { Route } from "./+types/fileCheck";
 import { listS3Objects } from "~/s3.server";
 
@@ -6,7 +5,7 @@ export function meta() {
   return [{ title: "Files - Admin Panel" }];
 }
 
-export async function loader() {
+export async function loader({}: Route.LoaderArgs) {
   return listS3Objects();
 }
 
