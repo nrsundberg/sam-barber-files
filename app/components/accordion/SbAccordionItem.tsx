078aafcc6add1735f711b596893f915c127cd6cb
nrsundberg <nrsundbe@ncsu.edu>
2025-03-18 10:00:27 -0400

    grid 2 for mobile

--- app/components/accordion/SbAccordionItem.tsx
+++ app/components/accordion/SbAccordionItem.tsx
@@ -254,7 +254,7 @@ export default function SbAccordionItem({
                 ))}
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 accordion-content">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 accordion-content">
                 {itemsToRender.map((object, objectIndex) => (
                   <ObjectGridLayout
                     key={object.id}
