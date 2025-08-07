c2bae62a61a3c7fde1568c32a0f1962c1a19b878
Noah Sundberg <noahsundberg@gmail.com>
2025-08-07 17:54:06 -0400

    run migration

---
+++ prisma/migrations/20250807215358_del/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `miscString` on the `Object` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Object" DROP COLUMN "miscString";
